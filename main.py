30a9f7213dcd09396bedce1863c23eb47b7d9ba9
HARISSHEIKH99 <muhammadharissheikh1@gmail.com>
2025-03-02 01:39:52 +0500

    first python class

---
+++ main.py
@@ -0,0 +1,70 @@
+print("hello world!")
+
+# Data Types In Pytho
+# 1-numbers 
+# int
+age = 27
+print(age)
+
+print(type(age))
+
+# float
+weigth = 72.50
+print(weigth)
+
+print(type(weigth))
+
+
+# 2-string/str
+name = "Ayesha Haris"
+print(name)
+
+print(type(name))
+
+# 3-boolean
+is_girl = True
+print(is_girl)
+
+print(type(is_girl))
+
+# 4-list
+list = [100,200,300]
+print(list)
+
+print(type(list))
+
+# 5 dictionary
+dict1 = {'name' :" ayesha",'age':27 , 'city': 'karachi'}
+print(dict1)
+
+print(type(dict1))
+
+# 6-tuples
+tup = (10,20,30)
+print(tup)
+
+print(type(tup))
+
+# 7 set
+
+set1 = {1,2,3,4,5,}
+print(set)
+
+print(type(set))
+
+# 9 None
+house_n0 = None
+print(house_n0)
+
+print(type(house_n0))
+
+# 10 range
+num = range(1,5)
+print(num)
+
+print(type(num))
+
+
+
+
+
